236678033eb710883441e2dcf65b7ad346b3adad
thierno sadou diallo <thierved01@gmail.com>
2019-04-08 15:14:28 +0000

    added redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import './index.css';
+
+const store = createStore(() => {});
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
